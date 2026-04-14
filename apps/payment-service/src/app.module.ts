cbfb693b91ec83bcb6a409643c417bc3c8584df2
luchersou <195580638+luchersou@users.noreply.github.com>
2026-04-14 15:12:28 -0300

    refactor(payment-service): reorganize payment module folder structure

--- apps/payment-service/src/app.module.ts
+++ apps/payment-service/src/app.module.ts
@@ -1,26 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { PrismaModule } from '@payment/prisma/prisma.module';
 
-import { PaymentConsumer } from './modules/payment/payment.consumer';
-import { PaymentsController } from './modules/payment/payment.controller';
-import { PaymentService } from './modules/payment/payment.service';
+import { PaymentModule } from './modules/payment/payment.module';
 import { paymentRabbitmqConfig } from './rabbitmq.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-
     RabbitMQModule.forRootAsync({
       useFactory: () => ({
         ...paymentRabbitmqConfig,
       }),
     }),
+    PaymentModule,
   ],
-  controllers: [PaymentsController],
-  providers: [PaymentService, PaymentConsumer],
 })
 export class AppModule {}
